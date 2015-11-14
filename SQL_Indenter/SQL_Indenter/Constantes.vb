2a559a159bd9fba083c0a8faf5ac2ee9888eae3f
AndersonLuciano <aluciano.net@gmail.com>
2015-11-14 21:04:52 -0200

    Adicionando arquivo.

---
+++ SQL_Indenter/SQL_Indenter/Constantes.vb
@@ -0,0 +1,18 @@
+﻿Public Class Constantes
+#Region "Statements"
+    Public Const SELECT_STATEMENT As String = "SELECT"
+    Public Const FROM_STATEMENT As String = "FROM"
+    Public Const WHERE_STATEMENT As String = "WHERE"
+#End Region
+
+
+    Public Const SELECT_COLUMN As String = "SELECT_COLUMN"
+
+#Region "Spaces"
+    Public Const SELECT_SPACES As Integer = 7
+    Public Const FROM_SPACES As Integer = 2
+    Public Const WHERE_SPACES As Integer = 1
+#End Region
+
+
+End Class
